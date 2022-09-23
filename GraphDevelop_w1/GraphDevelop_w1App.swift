fc62ceeca4036704bf2896cb680516e7bdec63bd
illgonggu <rbfl2365@icloud.com>
2022-09-23 21:47:08 +0900

    Initial Commit

---
+++ GraphDevelop_w1/GraphDevelop_w1App.swift
@@ -0,0 +1,17 @@
+//
+//  GraphDevelop_w1App.swift
+//  GraphDevelop_w1
+//
+//  Created by Loopy on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GraphDevelop_w1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
